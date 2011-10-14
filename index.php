52bd2be74cea92479d806c0b1bba07d83fab91a7
Hank Gupton <github@hankgupton.com>
2011-10-14 15:20:15 -0700

    Added "index.php" file to project.

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+<title>AzPHP Wish List API</title>
+<meta name="generator" content="Bluefish 2.0.3" >
+<meta name="author" content="Hank Gupton" >
+<meta name="date" content="2011-10-14T15:12:28-0700" >
+<style type="text/css">
+h1 {text-align: center; }
+</style>
+</head>
+<body>
+<h1>AzPHP Wish List API</h1>
+<h2>Common Starting Points</h2>
+<ul>
+  <li><a href="yiipath/demos/hangman">Hangman Game</a>: a demo game from Yii's framework.</li>
+  <li><a href="yiipath/testdrive/">Test Drive</a>:  proof that the Yii framework is alive.</li>
+  <li><a href="kohana/" >Environment Tests</a>: proof that the Kohana framework is alive.</li>
+</ul>
+</body>
+</html>
